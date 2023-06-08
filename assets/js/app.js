d3f40b78781057f9fc0e6e8ad6cc7f0fc5ba29dd
soxzen <soxzen6@gmail.com>
2023-06-08 12:55:13 +0200

    final finish ITS WORKKKKK

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,49 @@
-const dialog=[{question:"salut",answer:"Salut"},{question:"toto",answer:"titi"},{question:"La terre est ronde?",answer:"Non, la terre est plate dirigé par des réptiliens"}]
-//console.table(dialog)
-//console.log(document.getElementById("dialogchat"))
-document.getElementById("dialogchat").addEventListener("submit",function(event){
-    event.preventDefault();
-    //console.log(even.target);
-    document.querySelector(".question").innerHTML = document.querySelector(".ask").value
+const datalist = document.querySelector("#datalist");
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
+const inputdialog = document.querySelector("#inputdialog");
 
-dialog.forEach(Element =>{
-//console.log(Element.question)
-if(Element.question === document.querySelector(".ask").value){
-//console.log(Element.answer)
-document.querySelector(".answer").innerHTML=Element.answer
+let contentWelcome = document.querySelector("#welcome")
+const word = contentWelcome.textContent;
+const length = word.length;
+let count = 0;
+contentWelcome.textContent = "";
 
-}
-});
+const write = setInterval(() => {
+  contentWelcome.textContent += word[count]
+  count += 1;
+  if(count > length - 1) {
+    clearInterval(write)
+  }
+  console.log(count);
+}, 80);
 
+fetch("https://back-end-taupie-le-bot-mg0w.onrender.com/api/v1/dialogs")
+.then(response=>response.json())
+.then(({data})=>{
+    console.log(data);
+    data.forEach(dialog => {
+        datalist.innerHTML += `<option value="${dialog.question}">`
+    })
+
+    inputdialog.addEventListener("change", e=>{
+        question.innerHTML=document.querySelector(".form-question").value
+        data.forEach(dialog =>{
+            if(e.target.value === dialog.question){
+                console.log(dialog.answer)
+                answer.innerHTML = dialog.answer
+            }
+        })
+    })
 })
+.catch(error=>console.log(error))
+
+const chuck = document.getElementById("chuck")
+fetch("https://api.chucknorris.io/jokes/random")
+.then(response =>response.json())
+.then(data=>chuck.innerHTML = data.value)
+.catch(error => console.log(error))
+
+
+const data = fetchUrl("https://api.chucknorris.io/jokes/random")
+.then(data => console.log("then", data))
